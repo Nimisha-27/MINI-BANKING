972bf180620e0f9bc902c8a29d33d611ea131bf2
19euec024 <19euec024@skcet.ac.in>
2022-05-10 14:43:08 +0530

    crystaldelta

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Switch,Route} from 'react-router-dom'
+import Login from './Pages/Login/Login';
+const App = () =>{
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={Login}/>
+      </Switch>
+    </div>
+  )
+}
+
+export default App;
